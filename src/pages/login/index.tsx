5b5c25bb5ba6e9dae17ca100ee9e60bede399d82
hacxy <hacxy.js@outlook.com>
2025-12-01 15:34:10 +0800

    refactor: restructure application to use react-router for navigation, remove unused CSS files, and add login and welcome pages

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import { LoginFormPage, PageContainer } from '@ant-design/pro-components';
+
+function Login() {
+  return (
+    <PageContainer>
+      <LoginFormPage />
+    </PageContainer>
+  );
+}
+
+export default Login;
